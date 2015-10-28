a3191dbe15b98c4d27fd01e170747252ee5d1036
rachelsoh87 <ahsoh87@yahoo.com.my>
2015-10-28 04:33:59 +0000

    added landing page, sign up and login

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-
+gem "byebug"
 # Ruby Version
 # ruby "2.2.1"
 
---
+++ app/controllers/users.rb
@@ -0,0 +1,34 @@
+require "byebug"
+
+get '/' do #at the landing page
+
+  erb :"/static/index"
+end
+
+get "/signup" do #takes users from the landing page to the signup page
+
+  erb :"signup_page"
+end
+
+post "/signup" do #create a user
+    user_exists = User.find_by(user_email: params[:user_email])
+  
+  if user_exists.nil?
+    user = User.new(user_full_name: params[:user_full_name], user_password: params[:user_password], user_email: params[:user_email], self_description: params[:self_description]) 
+    url.save
+    redirect to "/login"
+
+  else 
+    redirect to "/login"
+  end
+end
+
+get "/login" do #show the login page
+  
+  erb :"login_page"
+end
+
+post "/login" do #submit details to compare to the table info
+
+erb :"login_page"
+end
